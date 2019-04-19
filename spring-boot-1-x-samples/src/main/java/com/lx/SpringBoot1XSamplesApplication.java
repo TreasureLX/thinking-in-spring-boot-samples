c8257a2b8daa199c704778b5437c38df63ff39b9
lanxing <1130876885@qq.com>
2019-04-19 22:11:54 +0800

    项目结构

---
+++ spring-boot-1-x-samples/src/main/java/com/lx/SpringBoot1XSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1XSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1XSamplesApplication.class, args);
+	}
+
+}
---
+++ spring-boot-2-x-samples/src/main/java/com/lx/SpringBoot2XSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2XSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2XSamplesApplication.class, args);
+	}
+
+}
