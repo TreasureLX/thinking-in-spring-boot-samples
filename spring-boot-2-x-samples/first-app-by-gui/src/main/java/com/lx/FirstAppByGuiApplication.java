b5e0fc89558a6ac2e3b8746ce93973c4acc4b88a
lanxing <1130876885@qq.com>
2019-04-22 20:17:56 +0800

    t

--- spring-boot-2-x-samples/first-app-by-gui/src/main/java/com/lx/FirstAppByGuiApplication.java
+++ spring-boot-2-x-samples/first-app-by-gui/src/main/java/com/lx/FirstAppByGuiApplication.java
@@ -2,12 +2,27 @@ package com.lx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class FirstAppByGuiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstAppByGuiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FirstAppByGuiApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> helloWorld() {
+        return route(GET("/hello-world"),
+                request -> ok().body(Mono.just("Hello,World"), String.class)
+        );
+    }
 
 }
